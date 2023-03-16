1d21a92ecd74aeba8528f989e8f53b24e753c628
BajakiGabesz <bajakigabesz@gmail.com>
2023-03-16 11:57:58 +0100

    Fix controls on ios

--- MediaManager/Platforms/Ios/Video/VideoView.cs
+++ MediaManager/Platforms/Ios/Video/VideoView.cs
@@ -30,6 +30,25 @@ namespace MediaManager.Platforms.Ios.Video
                 if (_playerViewController != null)
                 {
                     _playerViewController.View.Frame = Bounds;
+
+                    if (UIDevice.CurrentDevice.CheckSystemVersion(16, 0))
+                    {
+                        var viewController = GetCurrentUIViewController() ?? throw new InvalidOperationException("ViewController can't be null.");
+
+                        if (viewController.View is null)
+                        {
+                            throw new NullReferenceException($"{nameof(viewController.View)} cannot be null");
+                        }
+
+                        // Zero out the safe area insets of the AVPlayerViewController
+                        UIEdgeInsets insets = viewController.View.SafeAreaInsets;
+                        _playerViewController.AdditionalSafeAreaInsets =
+                            new UIEdgeInsets(insets.Top * -1, insets.Left, insets.Bottom * -1, insets.Right);
+
+                        // Add the View from the AVPlayerViewController to the parent ViewController
+                        viewController.View.AddSubview(_playerViewController.View);
+                    }
+
                     AddSubview(_playerViewController.View);
                     (Superview?.NextResponder as UIViewController)?.AddChildViewController(_playerViewController);
                 }
@@ -63,6 +82,75 @@ namespace MediaManager.Platforms.Ios.Video
         {
             if (MediaManager.MediaPlayer.AutoAttachVideoView)
                 MediaManager.MediaPlayer.VideoView = this;
+        }
+
+        public UIViewController? GetCurrentUIViewController()
+        {
+            UIViewController viewController = null;
+
+            var window = GetKeyWindow();
+
+            if (window != null && window.WindowLevel == UIWindowLevel.Normal)
+                viewController = window.RootViewController;
+
+            if (viewController == null)
+            {
+                window = GetWindows()?
+                    .OrderByDescending(w => w.WindowLevel)
+                    .FirstOrDefault(w => w.RootViewController != null && w.WindowLevel == UIWindowLevel.Normal);
+
+                viewController = window?.RootViewController;
+            }
+
+            while (viewController?.PresentedViewController != null)
+                viewController = viewController.PresentedViewController;
+
+            return viewController;
+        }
+
+        public UIWindow? GetCurrentUIWindow()
+        {
+            var window = GetKeyWindow();
+
+            if (window != null && window.WindowLevel == UIWindowLevel.Normal)
+                return window;
+
+            if (window == null)
+            {
+                window = GetWindows()?
+                    .OrderByDescending(w => w.WindowLevel)
+                    .FirstOrDefault(w => w.RootViewController != null && w.WindowLevel == UIWindowLevel.Normal);
+            }
+
+            return window;
+        }
+
+        static UIWindow? GetKeyWindow()
+        {
+            // if we have scene support, use that
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                var scenes = UIApplication.SharedApplication.ConnectedScenes;
+                var windowScene = scenes.ToArray<UIWindowScene>().FirstOrDefault();
+                return windowScene?.Windows.FirstOrDefault();
+            }
+
+            // use the windows property (up to 13.0)
+            return UIApplication.SharedApplication.KeyWindow;
+        }
+
+        static UIWindow[]? GetWindows()
+        {
+            // if we have scene support, use that
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13, 0))
+            {
+                var scenes = UIApplication.SharedApplication.ConnectedScenes;
+                var windowScene = scenes.ToArray<UIWindowScene>().FirstOrDefault();
+                return windowScene?.Windows;
+            }
+
+            // use the windows property (up to 15.0)
+            return UIApplication.SharedApplication.Windows;
         }
     }
 }
